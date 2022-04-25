664614b2a7fbc4351574414a45dce2ed2b007e79
Pablo Andreu <pablo.andreu.martin@gmail.com>
2022-04-25 18:02:55 +0200

    style Treatment

--- metropolitan/routes/user.routes.js
+++ metropolitan/routes/user.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const User = require('../models/User.model')
+const { isLoggedIn, checkRole } = require('../middelware/auth')
 
-router.get('/users', (req, res, next) => {
+router.get('/users', isLoggedIn, (req, res, next) => {
     const roleCheck = req.session.currentUser.role === 'ADMIN' || 'MODERATOR'
 
     User
