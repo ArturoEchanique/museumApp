ed0f26cad60c60baf8c73b9e0efdd81b1f1731c4
Pablo Andreu <pablo.andreu.martin@gmail.com>
2022-04-27 13:52:03 +0200

    maps, markers and details

--- metropolitan/models/artGallery.js
+++ metropolitan/models/artGallery.js
@@ -1,9 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-
-// No es seguro que los parametros extraidos de la API tengan que estar en el modelo, en teoria se construye un
-//documento de tipo arte al crear una coleccion, y si guardamos ya estos parametros no habra que acceder a la API
-// cada vez que se visualice un arte
 const artGallerySchema = new Schema({
     name: {
         type: String,
