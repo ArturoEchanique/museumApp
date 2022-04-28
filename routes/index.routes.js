26c0ac8eb866fcf12004ec8cf256e38e5f8422cb
Pablo Andreu <pablo.andreu.martin@gmail.com>
2022-04-28 21:09:29 +0200

    loosen work

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -14,34 +14,34 @@ const wikipediaAPI = new WikipediaHandler();
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  const collectionData = {}
-  collectionData.artItems = []
-  const { collectionId } = req.params
-  Collection
-    .findOne()
-    .populate("artItemsList")
-    .then(collection => {
-      collectionData.collection = collection
-      let artApiIds = collection.artItemsList.map(artItem => artItem.apiId)
-      let maxItemsToSlice = artApiIds.length
-      if (maxItemsToSlice % 2 == 1) maxItemsToSlice -= 1
-      artApiIds = artApiIds.slice(0, maxItemsToSlice)
-      const promisesArr = artApiIds.map(id => metAPI.getOneArtwork(id))
-      return Promise.all(promisesArr)
-    })
-    .then(responses => responses.map(elm => elm.data))
-    .then(artApiItems => {
-      collectionData.artApiItems = artApiItems
-      const promisesArr = artApiItems.map(artApiItem => ArtItem.findOne({ apiId: artApiItem.objectID }).populate("comments"))
-      return Promise.all(promisesArr)
-    })
-    .then(artItems => {
-      console.log("el objeto es..", artItems)
-      collectionData.artItems = artItems
-      console.log("ESTO---------------------", collectionData.artItems[0].apiId)
-      res.render("index", collectionData)
-    })
-    .catch(err => next(err))
+    const collectionData = {}
+    collectionData.artItems = []
+    const { collectionId } = req.params
+    Collection
+        .findOne()
+        .populate("artItemsList")
+        .then(collection => {
+            collectionData.collection = collection
+            let artApiIds = collection.artItemsList.map(artItem => artItem.apiId)
+            let maxItemsToSlice = artApiIds.length
+            if (maxItemsToSlice % 2 == 1) maxItemsToSlice -= 1
+            artApiIds = artApiIds.slice(0, maxItemsToSlice)
+            const promisesArr = artApiIds.map(id => metAPI.getOneArtwork(id))
+            return Promise.all(promisesArr)
+        })
+        .then(responses => responses.map(elm => elm.data))
+        .then(artApiItems => {
+            collectionData.artApiItems = artApiItems
+            const promisesArr = artApiItems.map(artApiItem => ArtItem.findOne({ apiId: artApiItem.objectID }).populate("comments"))
+            return Promise.all(promisesArr)
+        })
+        .then(artItems => {
+            console.log("el objeto es..", artItems)
+            collectionData.artItems = artItems
+                // console.log("ESTO---------------------", collectionData.artItems[0].apiId)
+            res.render("index", collectionData)
+        })
+        .catch(err => next(err))
 })
 
 module.exports = router;
